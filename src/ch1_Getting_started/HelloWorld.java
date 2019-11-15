74fc3fa68fa7b02f90aa5f93cf771cd3cd2e1dde
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-11-15 15:20:23 +0200

    Getting started with Java

---
+++ src/ch1_Getting_started/HelloWorld.java
@@ -0,0 +1,21 @@
+package ch1_Getting_started;
+
+/**
+ * First Java program
+ *
+ * @author solcanm
+ * @version 1.0
+ * @since 2019-11-15
+ */
+public class HelloWorld {
+
+    /**
+     * This is an entry point method from which the JVM can run your program. Every Java program should have
+     * one.
+     *
+     * @param args an array of Strings
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/ch1_Getting_started/Member.java
@@ -0,0 +1,9 @@
+package ch1_Getting_started;
+
+/**
+ * @author solcanm
+ * @version 1.0
+ * @since 2019-11-15
+ */
+public class Member {
+}
