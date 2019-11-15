9e407fa1b11db9f978e146256666b161461986cb
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-11-15 15:22:41 +0200

    Using constructors, getter and javadoc in Java

--- src/ch1_Getting_started/Member.java
+++ src/ch1_Getting_started/Member.java
@@ -1,9 +1,78 @@
 package ch1_Getting_started;
 
+import java.util.StringJoiner;
+
 /**
+ * Model a member of a team.
+ *
  * @author solcanm
  * @version 1.0
  * @since 2019-11-15
  */
 public class Member {
+    private String name;
+    private String type;
+    private int level;
+    private int rank;
+
+    /**
+     * Constructor of the class.
+     *
+     * @param name  - the name of the member
+     * @param type  - the type of the member
+     * @param level - the level of the member
+     * @param rank  - teh rank of the member
+     */
+    public Member(String name, String type, int level, int rank) {
+        this.name = name;
+        this.type = type;
+        this.level = level;
+        this.rank = rank;
+    }
+
+    /**
+     * Get the name of the member
+     *
+     * @return the name of the member
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Get the type of the member
+     *
+     * @return the type of type member
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Get the level of the member
+     *
+     * @return the level of the member
+     */
+    public int getLevel() {
+        return level;
+    }
+
+    /**
+     * Get the rank of the member
+     *
+     * @return the rank of the member
+     */
+    public int getRank() {
+        return rank;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Member.class.getSimpleName() + "[", "]")
+                .add("name='" + name + "'")
+                .add("type='" + type + "'")
+                .add("level=" + level)
+                .add("rank=" + rank)
+                .toString();
+    }
 }
--- src/ch1_Getting_started/Team.java
+++ src/ch1_Getting_started/Team.java
@@ -1,9 +1,65 @@
 package ch1_Getting_started;
 
+import java.util.StringJoiner;
+
 /**
+ * Model a football team with one member.
+ *
  * @author solcanm
  * @version 1.0
  * @since 2019-11-15
  */
 public class Team {
+
+    private Member member;
+
+    /**
+     * Instantiating the team with one member
+     *
+     * @param member the {@link Member} of the team
+     */
+    private Team(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Team.class.getSimpleName() + "[", "]")
+                .add("member=" + member)
+                .toString();
+    }
+
+    /**
+     * Create a member
+     * @return the created member
+     */
+    private static Member createMember() {
+        return new Member("Player1", "football", 23, 89);
+    }
+
+    /**
+     * Create a team and add a member to it.
+     * @return the created team
+     */
+    private static Team createTeam() {
+        Member football_member = createMember();
+        return new Team(football_member);
+    }
+
+    /**
+     * Display team members
+     */
+    private static void displayTeam(final Team team) {
+        System.out.println(team);
+    }
+
+    /**
+     * Entry method
+     * @param args te arguments from the command line
+     */
+    public static void main(String[] args) {
+        Team team = createTeam();
+        displayTeam(team);
+    }
+
 }
