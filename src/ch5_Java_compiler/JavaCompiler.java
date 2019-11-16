f0bd14af692f3873e2c785e64166225dd0d7b8a9
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-11-16 12:51:56 +0200

    Java Compiler - javac

---
+++ src/ch5_Java_compiler/JavaCompiler.java
@@ -0,0 +1,16 @@
+package ch5_Java_compiler;
+
+/**
+ * @author solcanm
+ * @version 1.0
+ * @since 2019-11-16
+ */
+public class JavaCompiler {
+    /**
+     * Assuming that we have {@link ch1_Getting_started.HelloWorld}
+     * We can compile the above file using this command:
+     *      - javac HelloWorld.java
+     * This produces a file called HelloWorld.class, which we can then run as follows:
+     *      - java HelloWorld
+     */
+}
