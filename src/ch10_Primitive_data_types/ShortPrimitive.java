4956b70c28a88d3fe41cc451ec3d8f1a3d5ea867
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-11-17 15:27:24 +0200

    Primitive type: short

---
+++ src/ch10_Primitive_data_types/ShortPrimitive.java
@@ -0,0 +1,16 @@
+package ch10_Primitive_data_types;
+
+/**
+ * A short is a 16-bit signed integer.
+ *
+ * @author solcanm
+ * @version 1.0
+ * @since 2019-11-17
+ */
+public class ShortPrimitive {
+
+    public static void main(String[] args) {
+        short minVal = Short.MIN_VALUE;
+        short maxVal = Short.MAX_VALUE;
+    }
+}
