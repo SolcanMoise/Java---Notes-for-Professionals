718afd56f8c7e4b29b6402daaed33c340258ab79
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-11-17 15:25:15 +0200

    Primitive type: byte

---
+++ src/ch10_Primitive_data_types/BytePrimitive.java
@@ -0,0 +1,16 @@
+package ch10_Primitive_data_types;
+
+/**
+ * A byte is a 8-bit signed integer.
+ *
+ * @author solcanm
+ * @version 1.0
+ * @since 2019-11-17
+ */
+public class BytePrimitive {
+
+    public static void main(String[] args) {
+        byte minVal = Byte.MIN_VALUE; // -128
+        byte maxVal = Byte.MAX_VALUE; // 127
+    }
+}
