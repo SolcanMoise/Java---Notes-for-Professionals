8ab890ec87b8e8b02497c9dc503c91b8cf2b0c4a
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-11-17 15:22:44 +0200

    Primitive type: boolean

---
+++ src/ch10_Primitive_data_types/BooleanPrimitive.java
@@ -0,0 +1,20 @@
+package ch10_Primitive_data_types;
+
+/**
+ * A boolean can store one of two values, either true or false.
+ *
+ * @author solcanm
+ * @version 1.0
+ * @since 2019-11-17
+ */
+public class BooleanPrimitive {
+
+    public static void main(String[] args) {
+        boolean trueVal = true;
+        boolean falseval = false;
+        boolean notTrue = !true;
+        boolean trueAndFalse = true && false;
+        boolean trueOrFalse = true || false;
+        boolean trueXorFalse = true ^ false;
+    }
+}
