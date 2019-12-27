a63d54e2d5cbc3af31a3aaac8098859b15db2877
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-12-27 21:38:19 +0200

    Expressing the power of 2

--- src/ch22_BitManipulation/PowerOf2.java
+++ src/ch22_BitManipulation/PowerOf2.java
@@ -17,9 +17,25 @@ public class PowerOf2 {
         int number = 8;
         boolean isPowerOfTwo = isPowerOfTwo(number);
         System.out.println(number + " is power of 2: " + isPowerOfTwo);
+
+        expressingPowerOfTwo();
     }
 
     private static boolean isPowerOfTwo(final int i) {
         return (i != 0) && (i & (i - 1)) == 0;
+    }
+
+    /**
+     * For expressing the power of 2(2^n) of integers, one may use a bitshift operation that allows
+     * to explicitly specify the n.
+     * the syntax is basically: int pow2 = 1 << n;
+     */
+    private static void expressingPowerOfTwo() {
+
+        int twoExp4 = 1 << 4; // 2^4
+        int twoExp5 = 1 << 5; // 2^5
+        int twoExp6 = 1 << 6; // 2^6
+        int twoExp31 = 1 << 31; // 2^31
+        System.out.println("2^5 = " + twoExp5);
     }
 }
